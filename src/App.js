548591f4bb79f30bea72bf8fd17cf09f494ca91d
Azim <azimrahat108@gmail.com>
2024-03-23 18:21:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css'
+import Navbar from './components/navbar';
+import StarrySky from './components/starrynight';
+import About from './components/about';
+import React from 'react';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <div class = 'secondBackground'>
+        <StarrySky/>
+        
+        <div class = 'navBehavior'> 
+          <Navbar/>
+        </div>
+
+        <div className = 'aboutBehavior'> 
+          <About/>
+          <About/>
+        </div>
+
+      </div>
     </div>
+
   );
 }
 
