d6425a744b637d11688fd241d8dd3e5263f35290
Azim <azimrahat108@gmail.com>
2024-04-02 01:57:39 -0400

    Changed project section. Started contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import '../styles/contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className = 'contactContainer'>
+
+      <h1 id = 'contact' className = 'title'>
+        <span className = 'contactHead'>
+          Contact Me
+        </span>
+      </h1>
+
+      <div className = 'contactGrid'>
+
+        <div className = 'contact-card'>
+          <span className = 'prompt'>
+            Your Name:
+          <input className = 'field'></input>
+          </span>
+
+          <span className = 'prompt'>
+            Your Email:
+          <input className = 'field'></input>
+          </span>
+
+          <span className = 'prompt'>
+            Your Message:
+          <input className = 'message-field'></input>
+          </span>
+        </div>
+
+        <div>
+          Hello
+        </div>
+
+      </div>
+
+
+
+
+
+
+    </div>
+
   )
 }
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -15,35 +15,44 @@ const Projects = () => {
 
       <div className = 'projectsList'>
 
-        <div className = 'project-box'>
-            <span>Portfolio Website</span>
-            <img src = {portfolioProject} alt = 'First Project'></img>
-            <p> This is my portfolio website built using React!
+        <div className = 'project-card'>
+          <img src = {portfolioProject} alt = 'project 1'></img>
+          <div className = 'content-slate'>
+            <a href = './'>My Portfolio</a>
+            <p>
+              Testing this out to make it look nice and presentable for the future!
             </p>
+            
             <div className = 'project-stack'>
               <div>JavaScript</div>
               <div>React</div>
               <div>HTML</div>
               <div>CSS</div>
             </div>
+          </div> 
         </div>
 
-        <div className = 'project-box'>
-            <span>WIP</span>
-            <img src = {project1} alt = 'First Project'></img>
-            <p> A full stack application
+        <div className = 'project-card'>
+          <img src = {project1} alt = 'project 2'></img>
+          <div className = 'content-slate'>
+            <a href = './'>A Work in Progress</a>
+            <p>
+              Testing this out to make it look nice and presentable for the future! 
+              This project will be a full stack application! TEST TEST TEST TEST TEST
             </p>
+            
             <div className = 'project-stack'>
               <div>JavaScript</div>
               <div>React</div>
               <div>HTML</div>
               <div>CSS</div>
             </div>
+          </div> 
         </div>
 
       </div>
-                  
     </div>
+                  
   )
 }
 
