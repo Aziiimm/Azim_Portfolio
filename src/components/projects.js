ad508cfa17def8a0d95028910b9b2a591342520d
Aziiimm <azimrahat108@gmail.com>
2024-04-06 22:40:10 -0400

    Fixed project links. Deployed Site.

--- src/components/projects.js
+++ src/components/projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
         <div className="project-card">
           <img src={calculatorproject} alt="project 1"></img>
           <div className="content-slate">
-            <a href="./">Calcify</a>
+            <a href="https://github.com/Aziiimm/Calcify">Calcify</a>
             <p>A calculator capable of solving basic calculus functions!</p>
 
             <div className="project-stack">
@@ -29,7 +29,7 @@ const Projects = () => {
         <div className="project-card">
           <img src={textgame} alt="project 2"></img>
           <div className="content-slate">
-            <a href="./">Text-Based Adventure Game</a>
+            <a href="https://github.com/Aziiimm/Text-Based-Adventure-Game">Text-Based Adventure Game</a>
             <p>
               A story driven adventure game where users navigate through a
               fantasy world with different path options.
