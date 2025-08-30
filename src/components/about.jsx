0e44ed8ad4ffb3121a1c30f502c727d53706f777
Azim Rahat <azimrahat108@gmail.com>
2025-08-30 16:22:08 -0400

    updated resume

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -49,7 +49,7 @@ const About = () => {
               </a>
               , and my&nbsp;
               <a
-                href="https://drive.google.com/file/d/11gOPVN55fG8DB0N2oXBOWj5teiJQ02Yg/view?usp=sharing"
+                href="https://drive.google.com/file/d/1PYzgxmwO4WVs1PKDupPnW8YVmuQoXPy4/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className=""
